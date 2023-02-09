7b1e2dd4fbca947001c798714565752219865abe
vadim3000 <vadim.golban@amdaris.com>
2023-02-09 11:03:10 +0200

    added changes in the userJourneyFile

--- cypress/e2e/shop/userJourney.cy.js
+++ cypress/e2e/shop/userJourney.cy.js
@@ -21,24 +21,32 @@ describe('User journey on the web site', ()=>{
     })
 
 
-    it.only("The user search for in the category for the product", ()=>{
+    it.only("The user search for the product in the category nav bar", ()=>{
         cy.get("span").contains(" Toate produsele ").trigger("mouseover")
         cy.get("span").contains(" Apple ")
-            // .realHover('mouse')
-            .trigger("mouseright", { which: 1, pageX: 600, pageY: 600 ,force:true })
-            // .invoke('show')
+            .click({force:true})
             //.trigger("mouseover", {force:true})
+
+            //cy.get("img[alt='Apple']").eq(2)
+            // .trigger('mouseover', {force:true})
+            // .click({force:true})
+
+            // .realHover('mouse')
+            // .trigger("mouseright", { which: 1, pageX: 600, pageY: 600 ,force:true })
+            // .invoke('show')
             // .click({force:true})
             // .url().should('include','https://maximum.md/ro/Apple/')
             .wait(1000)
-        // cy.get("span").contains(" Apple ").click({force:true})
+
+
+        cy.get("span").contains(" Apple ")
+
         // cy.get("span").contains(" Apple ").realHover('mouse')
         //cy.get("span").contains(" Apple ").realClick({force:true})
 
 
         // cy.get("a").contains(" Smartphone Apple iPhone ").trigger("mouseover",{force:true})
         // cy.get("a[data-id='40188']").contains(" Smartphone Apple iPhone ").click({force:true})
-
 
         // cy.get("span").contains(" Apple ").click({force:true})
         // cy.get("img[alt='Apple']").trigger("mouseover")
