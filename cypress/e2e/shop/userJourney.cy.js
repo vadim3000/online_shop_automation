b8992c87fa7cc744aaa2c8a155007926ab947f7c
vadim3000 <vadim.golban@amdaris.com>
2023-02-14 15:57:11 +0200

    made test on the userJourney file

--- cypress/e2e/shop/userJourney.cy.js
+++ cypress/e2e/shop/userJourney.cy.js
@@ -7,6 +7,37 @@ describe('User journey on the web site', ()=>{
         cy.openHomePage()
     })
 
+    // after('Test end', ()=>{
+    //     cy.end()
+    // })
+
+    it("The user changes the language on russian", ()=>{
+        cy.get("a[title='Schimbă limba']")
+            .click()
+            .wait(2000)
+    })
+
+    it('The user click on the logo icon', ()=>{
+        cy.get("img[title='Pagina principală']")
+            .click()
+    })
+
+    it("The user clicks on the next arrow on the carousel", ()=>{
+        cy.get("button[title='Next']")
+            .click()
+            .wait(300)
+            .click()
+            .wait(300)
+            .click()
+            .wait(300)
+            .click()
+            .wait(300)
+            .click()
+            .wait(300)
+            .click()
+            .wait(300)
+    })
+
     it("The user search a product in the search bar", ()=>{
         cy.get("input[title='Căutare']")
             .click()
@@ -15,16 +46,11 @@ describe('User journey on the web site', ()=>{
             .click()
     })
 
-    it('The user click on the logo icon', ()=>{
-        cy.get("img[title='Pagina principală']")
-            .click()
-    })
-
-
     it.only("The user search for the product in the category nav bar", ()=>{
         cy.get("span").contains(" Toate produsele ").trigger("mouseover")
         cy.get("span").contains(" Apple ")
             .click({force:true})
+            .wait(1000)
             //.trigger("mouseover", {force:true})
 
             //cy.get("img[alt='Apple']").eq(2)
@@ -36,20 +62,17 @@ describe('User journey on the web site', ()=>{
             // .invoke('show')
             // .click({force:true})
             // .url().should('include','https://maximum.md/ro/Apple/')
-            .wait(1000)
 
-
-        cy.get("span").contains(" Apple ")
-
+        // cy.get("span").contains(" Apple ")
         // cy.get("span").contains(" Apple ").realHover('mouse')
         //cy.get("span").contains(" Apple ").realClick({force:true})
 
-
         // cy.get("a").contains(" Smartphone Apple iPhone ").trigger("mouseover",{force:true})
         // cy.get("a[data-id='40188']").contains(" Smartphone Apple iPhone ").click({force:true})
-
-
     })
+
+
+
 
 })
 
