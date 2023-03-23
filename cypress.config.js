0d089c8d19e927962f2f19ff4181c12af6f15bb2
vadim3000 <vadim.golban@amdaris.com>
2023-03-23 12:09:24 +0200

    added updatges for sorry cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
+const { cloudPlugin } = require("cypress-cloud/plugin");
 
 module.exports = defineConfig({
   projectId: 'qcbygc',
+  // projectId: '2N8nSo',
 
   viewportHeight: 1080,
   viewportWidth: 1920,
@@ -9,6 +11,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return cloudPlugin(on, config);
     },
 
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
