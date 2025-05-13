07fc9aa584eb5a78881fea0588f5f0224d9d3a14
vadimAmd3000 <you@example.com>
2025-05-13 11:51:31 +0300

    added changes to UserJoureney.js file

--- cypress/e2e/shop/userJourney.js
+++ cypress/e2e/shop/userJourney.js
@@ -4,15 +4,18 @@ describe('User journey on the web site', function () {
     beforeEach('open homepage', function () {
         cy.openHomePage();
     });
+
     it("The user changes the language on russian", function () {
         cy.get("a[title='Schimbă limba']")
             .click()
             .wait(2000);
     });
+
     it('The user click on the logo icon', function () {
         cy.get("img[title='Pagina principală']")
             .click();
     });
+
     it("The user clicks on the next arrow on the carousel", function () {
         cy.get("button[title='Next']")
             .click()
@@ -23,6 +26,7 @@ describe('User journey on the web site', function () {
         // .wait(300)
         // .click()
     });
+
     it("The user search a product in the search bar", function () {
         cy.get("input[title='Căutare']")
             .click()
@@ -30,7 +34,8 @@ describe('User journey on the web site', function () {
         cy.get("button[title='Introduceți codul sau modelul produsului']")
             .click();
     });
-    it.only("The user search for the product in the category nav bar", function () {
+
+    it("The user search for the product in the category nav bar", function () {
         cy.get("span").contains(" Toate produsele ").trigger("mouseover");
         cy.get("span").contains(" Apple ")
             .click({ force: true });
